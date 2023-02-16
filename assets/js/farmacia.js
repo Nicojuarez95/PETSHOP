008b99336a7d698f4be8d3f8d51a6f795fa69e89
DanielOQ <danielorozco0208@gmail.com>
2023-02-15 20:52:39 -0500

    se sincroniza con la Api y se agregan los items

---
+++ assets/js/farmacia.js
@@ -0,0 +1,6 @@
+import {urlApi, getData, filterType, addItem} from "./module/funciones.js";
+let $containerProduct = document.querySelector(".contenedor-jf");
+getData(urlApi).then((data) => {
+  console.log(data);
+  addItem(filterType(data, "farmacia"), $containerProduct);
+});
---
+++ assets/js/module/funciones.js
@@ -0,0 +1,52 @@
+export const urlApi = "https://mindhub-xj03.onrender.com/api/petshop";
+export function filterType(list, type) {
+  return list.filter((item) => item.categoria === type);
+}
+export function addItem(list, element) {
+  element.innerHTML = "";
+  let template = "";
+  if (list.length != 0) {
+    for (let card of list) {
+      template += `<div class="product-card">
+                    <div class="${
+                      card.disponibles < 5 ? "badge" : "ocultar"
+                    }"</div>Ultimas unidades</div>
+                    <div class="product-tumb">
+                      <img
+                        src=${card.imagen}
+                        alt=${card.producto.replaceAll(" ", "")} />
+                    </div>
+                    <div class="product-details">
+                      <span class="product-catagory">Disponibles: ${
+                        card.disponibles
+                      }</span>
+                      <h4><a href="./detalles.html?id=${card._id}">${
+        card.producto
+      }</a></h4>
+                      <div class="product-bottom-details">
+                        <div class="product-price"><small>$${
+                          card.precio
+                        }</small>$${card.precio * 0.8}</div>
+                        <div class="product-links">
+                          <a href=""><i class="bi bi-cart"></i></a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>`;
+    }
+    element.innerHTML += template;
+  } else {
+    element.innerHTML = `<div id="notFound">
+      <h3><i class="bi bi-exclamation-square"></i> Producto no Encontrado</h3>
+    </div>`;
+  }
+}
+export async function getData(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
